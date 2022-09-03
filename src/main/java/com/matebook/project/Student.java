8e43060919c80cc4395cb4737ff025a94077178e
wangcao <945743736@qq.com>
2022-09-03 16:33:54 +0800

    分支Student edit

--- src/main/java/com/matebook/project/Student.java
+++ src/main/java/com/matebook/project/Student.java
@@ -6,7 +6,7 @@ package com.matebook.project;
  * @description:
  */
 public class Student {
- private String name12;
+ private String name1;
  private int age1;
  private int sex1;
  private String address;
