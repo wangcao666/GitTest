f12267a74ee1cf796be495745c282eac694871a6
wangcao <945743736@qq.com>
2022-09-03 13:28:22 +0800

    master 20220903 commit

---
+++ src/main/java/com/matebook/project/Teacher.java
@@ -0,0 +1,11 @@
+package com.matebook.project;
+
+/**
+ * @Author: wangcao
+ * @date: 2022/9/3 13:23
+ * @description:
+ */
+public class Teacher {
+    private String name;
+    private String address;
+}
